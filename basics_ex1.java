5a716304c8fd2dd9d3294d96406f0d70f166fb44
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-15 12:31:44 +0530

    Add files via upload
    in this we are going to create a pattern using different print()

---
+++ basics_ex1.java
@@ -0,0 +1,13 @@
+public class basics_ex1 {
+    public static void main(String[] args){
+        System.out.print("*");
+        System.out.print("\n* *");
+        System.out.print("\n* * *");
+
+        //by using println
+        System.out.println("\n*");
+        System.out.println("* *");
+        System.out.print("* * *");
+
+    }
+}
