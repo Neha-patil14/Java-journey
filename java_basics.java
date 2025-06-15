464c4c28f4022f17f8b140ca2921e0a8552bebae
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-15 12:30:14 +0530

    Add files via upload
    this our first java code nothing but "hello world"

---
+++ java_basics.java
@@ -0,0 +1,9 @@
+public class java_basics {
+    public static void main(String args[]){
+        System.out.println("Hello world"); //output in jva
+        System.out.println("Hello world");
+        System.out.print("Hello world");
+        System.out.print("\nHello world");
+        System.out.print("\nHello world");
+    }
+}
