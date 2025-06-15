bd2e78fc41c6016991a3f20d6e78aa118a86cb4b
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-15 12:34:13 +0530

    Add files via upload
    comments in java

---
+++ java_comment.java
@@ -0,0 +1,9 @@
+public class java_comment {
+    public static void main(String[] args) {
+        //type of comments
+        // single line 
+        /* 
+          multilinn ecomment
+         */
+    }
+}
