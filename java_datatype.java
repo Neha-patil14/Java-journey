ed092e5810e44917d36d480ebd0fa2d8069d5ec6
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-16 21:00:37 +0530

    Add files via upload
    which are the datatypes ,also their types are mentioned

---
+++ java_datatype.java
@@ -0,0 +1,16 @@
+public class java_datatype {
+   public static void main(String[] args){
+    byte b = 1;//1 byte
+    boolean k = false;// 1 byte
+    short c = 3;//2 byte
+    char ch = 'q';//2 byte
+    int a=4;//4 byte
+    long a = 6;//8 byte
+    float c = 6;//4  byte
+    double c = 6;//8 byte
+    /* types of datatype-
+         1.primitive:immutable
+         2.non-primitive:mutable
+     */
+   } 
+}
