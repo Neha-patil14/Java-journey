ff8212f05c86746c8c13e229936c6667662cb824
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-16 20:50:32 +0530

    Add files via upload
    how inputs are taken in java that is mentioned in this file

---
+++ java_input.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class java_input {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+       // String name = sc.next();
+        //System.out.println(name);
+
+        String name1 = sc.nextLine();
+        System.out.println(name1);
+        int num = sc.nextInt();
+        System.out.println(num);
+        float price = sc.nextFloat();
+        System.out.println(price);
+    }
+}
