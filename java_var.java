f76905070f4e08475773d16fdc51e549129b693d
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-06-18 23:52:12 +0530

    Add files via upload

---
+++ java_var.java
@@ -0,0 +1,11 @@
+public class java_var {
+    public static void main(String[] args){
+        int a = 2;// a is a variable
+        System.out.println(a);
+        a = 5;
+        System.out.println(a);
+        // 2 types of var 
+        // 1. literals- fixed such as value of ||pay 
+        //2. variables- changesble eg above value a 
+    }
+}
