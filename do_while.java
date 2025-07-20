f936535bb14feb5d6c1614eada43c9de1d557c3a
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-07-20 09:14:05 +0530

    Add files via upload

---
+++ do_while.java
@@ -0,0 +1,9 @@
+public class do_while {
+    public static void main(String[] args) {
+        int counter = 1;
+        do{
+            System.out.println("Hello");
+            counter++;
+        }while(counter<=5);
+    }
+}
