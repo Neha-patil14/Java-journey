484ca9317ec6cc2b54cae43d89931cff43c59013
Neha-patil14 <165042424+Neha-patil14@users.noreply.github.com>
2025-07-20 09:15:03 +0530

    Add files via upload

---
+++ for_loop.java
@@ -0,0 +1,8 @@
+public class for_loop {
+    public static void main(String[] args) {
+        //printing square pattern
+        for(int line =1;line<=4;line++){
+            System.out.println("* * * *");
+        }
+    }
+}
